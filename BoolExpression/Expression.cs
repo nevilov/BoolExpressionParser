4a72579c62943c7e2e6a2351f0eeb29f4d071a0f
Alim Iusufov <alim.iusufov@altusnova.com>
2021-10-24 11:06:17 +0300

    Add BoolExpression project with proccessing to tokens, polish notation and expression tree

---
+++ BoolExpression/Expression.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Expression
+    {
+        public enum BOP { LEAF, AND, OR, NOT };
+
+        private BOP _op;
+        private Expression _left;
+        private Expression _right;
+        private String _lit;
+
+        private Expression(BOP op, Expression left, Expression right)
+        {
+            _op = op;
+            _left = left;
+            _right = right;
+            _lit = null;
+        }
+
+        private Expression(String literal)
+        {
+            _op = BOP.LEAF;
+            _left = null;
+            _right = null;
+            _lit = literal;
+        }
+
+        public BOP Operation { get; set; }
+
+        public Expression Left { get; set; }
+
+        public Expression Right { get; set; }
+
+        public String Lit { get; set; }
+
+        public static Expression CreateAnd(Expression left, Expression right)
+        {
+            return new Expression(BOP.AND, left, right);
+        }
+
+        public static Expression CreateNot(Expression child)
+        {
+            return new Expression(BOP.NOT, child, null);
+        }
+
+        public static Expression CreateOr(Expression left, Expression right)
+        {
+            return new Expression(BOP.OR, left, right);
+        }
+
+        public static Expression CreateBoolVar(String str)
+        {
+            return new Expression(str);
+        }
+
+        public Expression(Expression other)
+        {
+            _op = other._op;
+            _left = other._left == null ? null : new Expression(other._left);
+            _right = other._right == null ? null : new Expression(other._right);
+            _lit = new StringBuilder(other._lit).ToString();
+        }
+
+        bool IsLeaf()
+        {
+            return (_op == BOP.LEAF);
+        }
+
+        bool IsAtomic()
+        {
+            return (IsLeaf() || (_op == BOP.NOT && _left.IsLeaf()));
+        }
+    }
+}
---
+++ BoolExpression/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = @"!((A&B)|C|D)";
+            List<Token> tokens = new();
+            StringReader reader = new StringReader(expression);
+
+            Token t = null;
+            do
+            {
+                t = new Token(reader);
+                tokens.Add(t);
+            } while (t.type != Token.TokenType.EXPR_END);
+
+            List<Token> polishNotation = TransformToPolishNotation(tokens);
+            var enumerator = polishNotation.GetEnumerator();
+            enumerator.MoveNext();
+            var proccessedExpresion = ProcessToExpression(ref enumerator);
+        }
+
+        static Expression ProcessToExpression(ref List<Token>.Enumerator notatedTokens)
+        {
+            if (notatedTokens.Current.type == Token.TokenType.LITERAL)
+            {
+                var expression = Expression.CreateBoolVar(notatedTokens.Current.value);
+                notatedTokens.MoveNext();
+                return expression;
+            }
+            else
+            {
+                if (notatedTokens.Current.value == "NOT")
+                {
+                    notatedTokens.MoveNext();
+                    Expression expression = ProcessToExpression(ref notatedTokens);
+                    return Expression.CreateNot(expression);
+                }
+                else if (notatedTokens.Current.value == "AND")
+                {
+                    notatedTokens.MoveNext();
+                    Expression left = ProcessToExpression(ref notatedTokens);
+                    Expression right = ProcessToExpression(ref notatedTokens);
+                    return Expression.CreateAnd(left, right);
+                }
+                else if (notatedTokens.Current.value == "OR")
+                {
+                    notatedTokens.MoveNext();
+                    Expression left = ProcessToExpression(ref notatedTokens);
+                    Expression right = ProcessToExpression(ref notatedTokens);
+                    return Expression.CreateOr(left, right);
+                }
+            }
+            return null;
+        }
+
+        static List<Token> TransformToPolishNotation(List<Token> infixTokenList)
+        {
+            Queue<Token> outputQueue = new Queue<Token>();
+            Stack<Token> stack = new Stack<Token>();
+
+            int index = 0;
+            while (infixTokenList.Count > index)
+            {
+                Token t = infixTokenList[index];
+
+                switch (t.type)
+                {
+                    case Token.TokenType.LITERAL:
+                        outputQueue.Enqueue(t);
+                        break;
+                    case Token.TokenType.BINARY_OP:
+                    case Token.TokenType.UNARY_OP:
+                    case Token.TokenType.OPEN_PAREN:
+                        stack.Push(t);
+                        break;
+                    case Token.TokenType.CLOSE_PAREN:
+                        while (stack.Peek().type != Token.TokenType.OPEN_PAREN)
+                        {
+                            outputQueue.Enqueue(stack.Pop());
+                        }
+                        stack.Pop();
+                        if (stack.Count > 0 && stack.Peek().type == Token.TokenType.UNARY_OP)
+                        {
+                            outputQueue.Enqueue(stack.Pop());
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                ++index;
+            }
+            while (stack.Count > 0)
+            {
+                outputQueue.Enqueue(stack.Pop());
+            }
+
+            return outputQueue.Reverse().ToList();
+        }
+
+    }
+}
